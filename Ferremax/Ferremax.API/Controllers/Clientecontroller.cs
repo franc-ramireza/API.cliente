a9ef1276a0b71938c94a07cc2b2d6135f9592b72
Francisca Ramirez <107446606+franc-ramireza@users.noreply.github.com>
2024-06-06 17:09:40 -0400

    appi avanzada

---
+++ Ferremax/Ferremax.API/Controllers/Clientecontroller.cs
@@ -0,0 +1,40 @@
+﻿using Ferremax.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ferremax.API.Controllers
+{
+    [ApiController]
+    [Route("Cliente")]
+    public class Clientecontroller: ControllerBase
+    {
+        [HttpGet]
+        [Route("Listar")]
+
+        public dynamic ListarCliente()
+        {
+            List<Cliente> cliente = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    razonSocial = "Ferreteria pepito Lmtada",
+                    rut = "121231231",
+                    direccion = " avenida vicuña mackenna 4860 , Macul"
+                },
+
+                new Cliente
+                {
+                    id = "2",
+                    razonSocial = "Ferreteria don gato Lmtada",
+                    rut = "122342341",
+                    direccion = "avenida cardenal caro 2140, Loprado"
+
+
+                },
+
+            };
+            return cliente;
+
+        }
+    }
+}
---
+++ Ferremax/Ferremax.API/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ferremax.API.Models
+
+{
+    public class Cliente
+    {
+        public String id { get; set; }
+        public string razonSocial { get; set; }
+
+        public string rut { get; set; }
+
+        public string direccion { get; set; }
+    }
+}
